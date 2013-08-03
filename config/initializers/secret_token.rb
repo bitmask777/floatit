5f838bec4e672e9dfcf962825c68211249ff1931
Robert Hartman <gate4hartmanr@gmail.com>
2013-08-02 17:18:34 -0700

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Floatit::Application.config.secret_key_base = '8e05e9e62519d173582e1edbcfd9d729cee0bc08b8da41106597e57e594f522a6f29e8be0c56912c6f9f78f51246dfb274dc6afff30a23b7630a9df24f83d949'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Floatit::Application.config.session_store :cookie_store, key: '_floatit_session'
