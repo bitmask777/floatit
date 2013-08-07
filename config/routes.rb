dda67d48c69c47e2451bf1ca363fa628a045ed50
Robert Hartman <gate4hartmanr@gmail.com>
2013-08-07 15:27:38 -0700

    another recommit. oops

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Floatit::Application.routes.draw do
+
+  get "static_pages/home", as: :home
+  get "static_pages/help", as: :help
+  get "static_pages/about", as: :about
+
   resources :items
 
   resources :users
